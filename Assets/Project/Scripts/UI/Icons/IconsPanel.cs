f201fdc4ba86451a3aa27f6a8fbf7871eb23f129
Lyahasik <48154267+Lyahasik@users.noreply.github.com>
2023-02-17 21:17:54 +0300

    added icons open/close map

--- Assets/Project/Scripts/UI/Icons/IconsPanel.cs
+++ Assets/Project/Scripts/UI/Icons/IconsPanel.cs
@@ -2,24 +2,31 @@ using UnityEngine;
 using Zenject;
 
 using Gameplay.Buffs;
+using FiniteStateMachine;
 
 namespace UI.Icons
 {
     public class IconsPanel : MonoBehaviour, IInitializable
     {
+        private DiContainer _container;
+        private GameMashine _gameMashine;
         private BuffsHandler _buffsHandler;
         
+        [SerializeField] private IconMap _iconMap;
         [SerializeField] private IconSpeedBuff _iconSpeedBuff;
         [SerializeField] private IconVisibilityRangeUpBuff _iconVisibilityRangeUpBuff;
 
         [Inject]
-        public void Construct(BuffsHandler buffsHandler)
+        public void Construct(DiContainer container, GameMashine gameMashine, BuffsHandler buffsHandler)
         {
+            _container = container;
+            _gameMashine = gameMashine;
             _buffsHandler = buffsHandler;
         }
 
         public void Initialize()
         {
+            _iconMap.Init(_container, _gameMashine);
             _iconSpeedBuff.Init(_buffsHandler);
             _iconVisibilityRangeUpBuff.Init(_buffsHandler);
         }
--- Assets/Project/Scripts/UI/Map/MapPanel.cs
+++ Assets/Project/Scripts/UI/Map/MapPanel.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
+using Zenject;
+
+using FiniteStateMachine;
 
 namespace UI.Map
 {
     public class MapPanel : MonoBehaviour
     {
+        private DiContainer _container;
+        private GameMashine _gameMashine;
+
+        [Inject]
+        public void Construct(DiContainer container, GameMashine gameMashine)
+        {
+            _container = container;
+            _gameMashine = gameMashine;
+        }
+
+        public void Deactivate()
+        {
+            _gameMashine.Enter(_container.Instantiate<PlayingState>());
+        }
+        
         public void Activate(bool value)
         {
             gameObject.SetActive(value);
